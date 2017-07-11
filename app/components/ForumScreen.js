6fe5fe8d8516c94714e4ea8aef478ff5236d7993
vimalN1992 <vimal.victory1100@gmail.com>
2017-07-11 11:53:01 +0530

    adding calender, forum and profile

--- app/components/ForumScreen.js
+++ app/components/ForumScreen.js
@@ -1,41 +1,111 @@
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native'; 
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { ActivityIndicator, View, Text, Image, TextInput, ScrollView, TouchableHighlight, ListView,StatusBar } from 'react-native';
 
-export default class ForumScreen extends Component {
-  static navigationOptions = { 
-    header: null,  
-  };
+import { List, ListItem } from 'react-native-elements'
+
+export default class ForumList extends Component {
+  constructor(props)
+    {
+        super(props);
+        const {state} = this.props.navigation;
+        // console.log(state.params.forum_data)
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+        
+        text: '',
+        dataSource: ds.cloneWithRows(state.params.forum_data),
+        };
+
+        this._renderRow = this._renderRow.bind(this);
+        this._pressRow = this._pressRow.bind(this);
+    }
+
+
+  _renderRow(data) {
+     // console.log(data);
+if(data != 'Forum not found for the particular course')
+        {
+
+        const d = new Date(data.timemodified * 1000).toDateString();
+        const total = '( '+data.tot+' )';
+    return ( 
+
+        <List containerStyle={{marginBottom: -18,marginRight:5,marginLeft:5,bottom:15}}>
+
+          <ListItem
+          roundAvatar
+          key={data.id}
+          title={data.name}
+          titleContainerStyle={{width:280,paddingBottom:5}}
+          subtitle={d}
+          rightTitle= {total}
+          onPress={() => this._pressRow(data) }
+          />
+
+      </List>
+
+                      
+    );
+
+     }
+        else
+        {
+            return (<Text style={{fontWeight: 'bold',textAlign:'center',color:'#34495e',paddingTop:150,fontSize:18}}>{data}</Text>);
+        }
+  }
+
+ _pressRow(forum){
+        console.log('Forum id - '+ forum.id);
+
+    const { navigate } = this.props.navigation;
+    const url = 'http://10.21.2.45:8001/app/discussionlist/'+forum.id ;
+   
+    fetch(url, {
+    method: 'GET',
+    })
+    .then((response) => { return response.json() } )                     //Fetch and passing data
+    .then((responseJson) => {
+    navigate('ForumTopics', { forumtopics_data: responseJson });
+    })
+    .catch((error) => {
+    Alert.alert("Network Not Reachable. Try Again");
+    });
+
+
+}
+
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Welcome to Forum</Text>
-      </View>
+     
+            <View style={{flex: 1, padding: 3 }}>
+                                
+                  <ListView
+                  dataSource={this.state.dataSource}
+                  renderRow={this._renderRow}  
+                  />        
+                    
+            </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+const styles = {
+    box:{
+        flexDirection: 'row', 
+        borderWidth: 1, 
+        padding: 10,
+        backgroundColor: '#fff',
+        margin:3,
+        }
+}
+
--- app/components/ProfileScreen.js
+++ app/components/ProfileScreen.js
@@ -1,42 +1,81 @@
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight
-} from 'react-native'; 
+import { View, Text, StyleSheet,TouchableHighlight, ScrollView } from 'react-native';
+import { Tile, List, ListItem, Button } from 'react-native-elements';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Menu, { MenuOptions, MenuOption, MenuTrigger } from 'react-native-menu';
+
+
+export const profile_data = {
+  "gender": "male",
+   "uname": "Vimalraj",
+   "phone": "9551234578",
+  "name": "Vimal N",
+  "email": "vimal@tenet.res.in", 
+  "picture": {
+    "large": "http://www.tenet.res.in/picture/136EE_user.png"
+  },
+ 
+};
+
 
 export default class ProfileScreen extends Component {
-  static navigationOptions =  {
-      header: null
+    
+  static navigationOptions = {
+    header: null
   }
+
+   handleSettingsPress = () => {
+    const edit_data = this.props;
+    this.props.navigation.navigate('Edit',{data: profile_data});
+  };
+
   render() {
+    
     return (
-      <View style={styles.container}>
-        <Text>Welcome to Profile</Text>
-      </View>
+      <ScrollView>
+          <Tile
+          imageSrc={{ uri: profile_data.picture.large}}
+          featured
+          title={`${profile_data.name.toUpperCase()}`}
+          caption={profile_data.email}
+          imageContainerStyle={{padding:200}}
+        />
+        <Button
+          title="EDIT"
+          buttonStyle={{ marginTop: 20,backgroundColor:"#1abc9c" }}
+          onPress={this.handleSettingsPress}
+        />
+        <List>
+          <ListItem
+            title="Email"
+            rightTitle={profile_data.email}
+            hideChevron
+          />
+          <ListItem
+            title="Phone"
+            rightTitle={profile_data.phone}
+            hideChevron
+          />
+        </List>
+        <List>
+          <ListItem
+            title="Username"
+            rightTitle={profile_data.uname}
+            hideChevron
+          />
+        </List>
+        <List>
+            <ListItem
+            title="Gender"
+            rightTitle={profile_data.gender}
+            hideChevron
+          />
+        </List>
+
+      </ScrollView>
+      
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
